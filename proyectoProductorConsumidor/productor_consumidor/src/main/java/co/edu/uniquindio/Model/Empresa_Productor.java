8c1d942e570913d2c06ef4134cf283202451e72a
JPabloLG <juanp.lopezg@uqvirtual.edu.co>
2024-10-30 11:45:05 -0500

    cambios

---
+++ proyectoProductorConsumidor/productor_consumidor/src/main/java/co/edu/uniquindio/Model/Empresa_Productor.java
@@ -0,0 +1,40 @@
+package co.edu.uniquindio.Model;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class Empresa_Productor {
+
+    private static final String QUEUE_NAME = "autos lavando";
+    private Tuberia tuberia;
+
+    public Empresa_Productor(Tuberia tuberia) {
+        this.tuberia = tuberia;
+        producirAutosLavando();
+    }
+
+    private void producirAutosLavando() {
+        String[] autosLavando = {"TTT111", "JQK779", "LUS358", "GJX152", "HTK465"};
+
+        try{
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("localhost");
+            try(Connection connection = factory.newConnection();
+                Channel channel = connection.createChannel()){
+
+                channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+                for (String auto : autosLavando){
+                    tuberia.producir(auto);
+
+                    String mensaje = "Auto producido" + auto;
+                    channel.basicPublish("", QUEUE_NAME, null, mensaje.getBytes());
+                    System.out.println(" Prdocutor envió mensaje a RabbitMQ: " + mensaje);
+                }
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
--- proyectoProductorConsumidor/productor_consumidor/src/main/java/co/edu/uniquindio/Model/Tuberia.java
+++ proyectoProductorConsumidor/productor_consumidor/src/main/java/co/edu/uniquindio/Model/Tuberia.java
@@ -1,5 +1,31 @@
 package co.edu.uniquindio.Model;
 
 public class Tuberia {
+    private Queue<String> buffer;
+    private int capacidad;
 
+    public Tuberia(int capacidad) {
+        this.capacidad = capacidad;
+        this.buffer = new LinkedList<String>();
+    }
+
+    public synchronized void producir(String placa) throws InterruptedException {
+        while(buffer.size == capacidad){
+            wait();
+        }
+        buffer.add(placa);
+        notifyAll();
+    }
+
+    public synchronized String consumir(String placaSolicitada){
+        if(buffer.contains(placaSolicitada)){
+            buffer.remove(placaSolicitada);
+            return placaSolicitada;
+        }
+        return null;
+    }
+
+    public synchronized boolean hayCarro(String placaSolicitada){
+        return buffer.contains(placaSolicitada);
+    }
 }
